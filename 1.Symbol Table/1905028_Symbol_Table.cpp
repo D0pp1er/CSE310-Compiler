3066284cd5770ba65f18df735f1925327f2fead7
Doppler <sharifulislam.si689@gmail.com>
2022-12-01 17:09:03 +0600

    Before lunch

--- 1.Symbol Table/1905028_Symbol_Table.cpp
+++ 1.Symbol Table/1905028_Symbol_Table.cpp
@@ -15,7 +15,7 @@ public:
     {
         this->name=name;
         this->type=type;
-        nxtptr=NULL;
+        nxtptr=nullptr;
     }
 
     Symbol_Info()
@@ -63,5 +63,106 @@ public:
         o<<"<"<<syminfo.name<<","<<syminfo.type<<">";
         return o;
     }
+
+};
+
+
+class Scope_Table{
+
+private:
+    int num_buckets,Scope_ID;
+    Symbol_Info** hash_table;
+    Scope_Table* parent_Scope;
+
+    int SDBM_Hash(string str)
+    {
+        unsigned int hash = 0;
+        unsigned int i = 0;
+        unsigned int len = str.length();
+
+        for (i = 0; i < len; i++)
+        {
+            hash = (str[i]) + (hash << 6) + (hash << 16) - hash;
+        }
+
+        return hash%num_buckets;
+    }
+
+public:
+
+
+    Scope_Table(int Scope_ID,int num_buckets,Scope_Table *parent_Scope)
+    {
+        this->Scope_ID=Scope_ID;
+        this->num_buckets=num_buckets;
+        this->parent_Scope=parent_Scope;
+
+        hash_table=new Symbol_Info*[num_buckets];
+
+        for (int i = 0; i < num_buckets; i++)
+        {
+            hash_table[i]=nullptr;
+        }
+    }
+
+    ~Scope_Table()
+    {
+        for (int i = 0; i < num_buckets; i++)
+        {
+            if(hash_table[i]!=nullptr)
+            {
+                Symbol_Info *temp1=hash_table[i];
+                while (temp1!=nullptr)
+                {
+                    Symbol_Info *temp2=temp1;
+                    temp1=temp1->get_nxtptr();
+                    delete temp2;
+                }
+            }
+        }
+
+        delete[] hash_table;
+        
+    }
+
+    int getID()
+    {
+        return Scope_ID;
+    }
+
+    int getLength()
+    {
+        return num_buckets;
+    }
+
+    Scope_Table *getParent()
+    {
+        return parent_Scope;
+    }
+
+   Symbol_Info *LookUpSymbol(string symbol_name)
+   {
+        int i= SDBM_Hash(symbol_name);
+        Symbol_Info *temp=hash_table[i];
+        int pos=1;
+
+        while (temp!=nullptr)
+        {
+            if(temp->getName()==symbol_name)
+            {
+                cout<<"\t"<<"'"<<symbol_name<<"' found in ScopeTable# "<<Scope_ID<<" at position "<<i<<", "<<pos<<endl;
+                return temp;
+            }
+            pos++;
+            temp=temp->get_nxtptr();
+        }
+        return nullptr;
+   }
+
+
+
+
+
+
 
 };
